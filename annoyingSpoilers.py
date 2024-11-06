51c218bbeffa4f523024239a92f6d2355f3f3074
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2024-11-06 19:22:39 +0800

    Create annoyingSpoilers.py

---
+++ annoyingSpoilers.py
@@ -0,0 +1,16 @@
+# -------------------------- Instructions --------------------------
+# 
+# Discord spoiler text generator
+# Run the program then input your message.
+# Spoilered message will be printed to terminal.
+# Every letter is spoilered spearately so it is annoying to 
+# unspoiler and read.
+# Paste into discord chats to annoy people.
+#
+# ------------------------------------------------------------------
+
+msg = input('Message: ')
+res = ''
+for i in msg:
+    res += f'||{i}||'
+print(res)
