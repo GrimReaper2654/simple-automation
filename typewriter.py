35d366e6af671284862ee1435a5e26183ec912cf
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2025-01-09 22:59:02 +0800

    new things

---
+++ typewriter.py
@@ -0,0 +1,16 @@
+# -------------------------- Instructions --------------------------
+# 
+# Run the program, it will write stuff. Edit the phrase to suit 
+# your needs.
+#
+# ------------------------------------------------------------------
+
+from helper import *
+import pyautogui
+import random
+import time
+
+phrase = 'hello world'
+
+time.sleep(1) # Adjust to suit your own reaction time
+pyautogui.typewrite(phrase)
